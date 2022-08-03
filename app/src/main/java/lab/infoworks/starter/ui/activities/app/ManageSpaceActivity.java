4c95aca6af3e0a35d22df5187f0f9ed45a0794f8
Towhidul Islam <m.towhid.islam@gmail.com>
2022-08-03 14:46:36 +0600

    adding manageSpaceActivity to prevent data clean up

---
+++ app/src/main/java/lab/infoworks/starter/ui/activities/app/ManageSpaceActivity.java
@@ -0,0 +1,15 @@
+package lab.infoworks.starter.ui.activities.app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManageSpaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish();
+    }
+}
